e25b5e5084837e7cf70ac9681f64732f23f327dd
Aman Tripathi <imamantripathi@gmail.com>
2023-03-19 22:19:37 +0530

    Modern Bank App

---
+++ bank_modern_app/src/components/Business.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { features } from "../constants";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const Business = () => {
+  return <section className={layout.section} id="features" >
+    <div></div>
+  </section>;
+};
+
+export default Business;
