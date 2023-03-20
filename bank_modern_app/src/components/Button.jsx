3fcd19f1aac64b82424932590f6dcecac0b08cbe
Aman Tripathi <imamantripathi@gmail.com>
2023-03-20 23:07:19 +0530

    complete till billing

--- bank_modern_app/src/components/Button.jsx
+++ bank_modern_app/src/components/Button.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import styles from "../style";
 
-const Button = () => {
+const Button = ({ styles }) => {
   return (
-    <div>Button</div>
-  )
-}
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-primary text-[18px] outline-none ${styles}`}
+    >
+      Get Started
+    </button>
+  );
+};
 
-export default Button
+export default Button;
