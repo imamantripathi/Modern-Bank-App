1236ea32f3c42f575ec9fc57d9367aaf7d1c1117
Aman Tripathi <imamantripathi@gmail.com>
2023-03-21 18:04:02 +0530

    Project Complete

--- bank_modern_app/src/components/Button.jsx
+++ bank_modern_app/src/components/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({ styles }) => {
   return (
     <button
       type="button"
-      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-primary text-[18px] outline-none ${styles}`}
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-primary text-[18px] outline-none ${styles} `}
     >
       Get Started
     </button>
